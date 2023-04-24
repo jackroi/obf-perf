6a60c4b7453a6f2707032953f9eeef6e643ff384
Giacomo Rosin <giacomorosin@gmail.com>
2023-04-24 12:27:44 +0200

    Add non recursive versions of merge and quick sort

--- examples/src-examples/merge_sort.c
+++ examples/src-examples/merge_sort.c
@@ -42,39 +42,73 @@ void print_array(int array[], int size) {
 }
 
 // merge sort
+/* void sort(int arr[], int len) { */
+/*   if (len < 2) { */
+/*     return; */
+/*   } */
+
+/*   int mid = len / 2; */
+/*   sort(arr, mid); */
+/*   sort(arr + mid, len - mid); */
+
+/*   int *temp = malloc(len * sizeof(int)); */
+/*   int i = 0; */
+/*   int j = mid; */
+/*   int k = 0; */
+
+/*   while (i < mid && j < len) { */
+/*     if (arr[i] <= arr[j]) { */
+/*       temp[k++] = arr[i++]; */
+/*     } else { */
+/*       temp[k++] = arr[j++]; */
+/*     } */
+/*   } */
+
+/*   while (i < mid) { */
+/*     temp[k++] = arr[i++]; */
+/*   } */
+
+/*   while (j < len) { */
+/*     temp[k++] = arr[j++]; */
+/*   } */
+
+/*   for (i = 0; i < len; i++) { */
+/*     arr[i] = temp[i]; */
+/*   } */
+
+/*   free(temp); */
+/* } */
+
+// non recursive merge sort
 void sort(int arr[], int len) {
-  if (len < 2) {
-    return;
-  }
-
-  int mid = len / 2;
-  sort(arr, mid);
-  sort(arr + mid, len - mid);
-
   int *temp = malloc(len * sizeof(int));
-  int i = 0;
-  int j = mid;
-  int k = 0;
-
-  while (i < mid && j < len) {
-    if (arr[i] <= arr[j]) {
-      temp[k++] = arr[i++];
-    } else {
-      temp[k++] = arr[j++];
+  int i, j, k, l, m, n;
+  for (i = 1; i < len; i *= 2) {
+    for (j = 0; j < len - i; j += 2 * i) {
+      k = j;
+      l = j + i;
+      m = j + 2 * i;
+      if (m > len) {
+        m = len;
+      }
+      n = j;
+      while (k < j + i && l < m) {
+        if (arr[k] <= arr[l]) {
+          temp[n++] = arr[k++];
+        } else {
+          temp[n++] = arr[l++];
+        }
+      }
+      while (k < j + i) {
+        temp[n++] = arr[k++];
+      }
+      while (l < m) {
+        temp[n++] = arr[l++];
+      }
+    }
+    for (j = 0; j < len; j++) {
+      arr[j] = temp[j];
     }
   }
-
-  while (i < mid) {
-    temp[k++] = arr[i++];
-  }
-
-  while (j < len) {
-    temp[k++] = arr[j++];
-  }
-
-  for (i = 0; i < len; i++) {
-    arr[i] = temp[i];
-  }
-
   free(temp);
 }
--- examples/src-examples/quick_sort.c
+++ examples/src-examples/quick_sort.c
@@ -42,27 +42,120 @@ void print_array(int array[], int size) {
 }
 
 // quick sort
-void sort(int arr[], int len) {
-  if (len < 2) {
-    return;
-  }
+/* void sort(int arr[], int len) { */
+/*   if (len < 2) { */
+/*     return; */
+/*   } */
 
-  int pivot = arr[len / 2];
-  int i, j;
-  for (i = 0, j = len - 1; ; i++, j--) {
-    while (arr[i] < pivot) {
-      i++;
+/*   int pivot = arr[len / 2]; */
+/*   int i, j; */
+/*   for (i = 0, j = len - 1; ; i++, j--) { */
+/*     while (arr[i] < pivot) { */
+/*       i++; */
+/*     } */
+/*     while (arr[j] > pivot) { */
+/*       j--; */
+/*     } */
+/*     if (i >= j) { */
+/*       break; */
+/*     } */
+/*     int temp = arr[i]; */
+/*     arr[i] = arr[j]; */
+/*     arr[j] = temp; */
+/*   } */
+/*   sort(arr, i); */
+/*   sort(arr + i, len - i); */
+/* } */
+
+// non recursive quick sort
+/* void sort(int arr[], int len) { */
+/*   int stack[len]; */
+/*   int top = -1; */
+/*   stack[++top] = 0; */
+/*   stack[++top] = len - 1; */
+
+/*   while (top >= 0) { */
+/*     int end = stack[top--]; */
+/*     int start = stack[top--]; */
+
+/*     if (start >= end) { */
+/*       continue; */
+/*     } */
+
+/*     int pivot = arr[end]; */
+/*     int i, j; */
+/*     for (i = start, j = end - 1; ; i++, j--) { */
+/*       while (arr[i] < pivot) { */
+/*         i++; */
+/*       } */
+/*       while (arr[j] > pivot) { */
+/*         j--; */
+/*       } */
+/*       if (i >= j) { */
+/*         break; */
+/*       } */
+/*       int temp = arr[i]; */
+/*       arr[i] = arr[j]; */
+/*       arr[j] = temp; */
+/*     } */
+/*     int temp = arr[i]; */
+/*     arr[i] = arr[end]; */
+/*     arr[end] = temp; */
+
+/*     stack[++top] = start; */
+/*     stack[++top] = i - 1; */
+/*     stack[++top] = i + 1; */
+/*     stack[++top] = end; */
+/*   } */
+/* } */
+
+#define MAX_LEVELS  48
+
+// non recursive quick sort (and fixed size stack)
+void sort(int arr[], int elements) {
+  size_t beg[MAX_LEVELS], end[MAX_LEVELS], L, R;
+  int i = 0;
+
+  beg[0] = 0;
+  end[0] = elements;
+  while (i >= 0) {
+    L = beg[i];
+    R = end[i];
+    if (R - L > 1) {
+      size_t M = L + ((R - L) >> 1);
+      int piv = arr[M];
+      arr[M] = arr[L];
+
+      if (i == MAX_LEVELS - 1)
+        return;
+      R--;
+      while (L < R) {
+        while (arr[R] >= piv && L < R)
+          R--;
+        if (L < R)
+          arr[L++] = arr[R];
+        while (arr[L] <= piv && L < R)
+          L++;
+        if (L < R)
+          arr[R--] = arr[L];
+      }
+      arr[L] = piv;
+      M = L + 1;
+      while (L > beg[i] && arr[L - 1] == piv)
+        L--;
+      while (M < end[i] && arr[M] == piv)
+        M++;
+      if (L - beg[i] > end[i] - M) {
+        beg[i + 1] = M;
+        end[i + 1] = end[i];
+        end[i++] = L;
+      } else {
+        beg[i + 1] = beg[i];
+        end[i + 1] = L;
+        beg[i++] = M;
+      }
+    } else {
+      i--;
     }
-    while (arr[j] > pivot) {
-      j--;
-    }
-    if (i >= j) {
-      break;
-    }
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
   }
-  sort(arr, i);
-  sort(arr + i, len - i);
 }
